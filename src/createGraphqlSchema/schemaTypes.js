78127374e781964555bdc10343629b11a9360c00
Adam Rackis <adamrackis@gmail.com>
2017-09-23 20:03:26 -0500

    snapshot of bug

---
+++ src/createGraphqlSchema/schemaTypes.js
@@ -0,0 +1,4 @@
+export const MongoId = "MongoId";
+export const String = "String";
+export const Int = "Int";
+export const Float = "Float";
--- test/module.js
+++ test/module.js
@@ -2,7 +2,4 @@ import path from "path";
 
 import { createGraphqlSchema } from "../index";
 
-//console.log(path.normalize("project/schema.js"));
-console.log(path.resolve("./project/schema.js"));
-
-createGraphqlSchema(import("./project/schema.js"));
+createGraphqlSchema(import("./project/schema.js"), path.resolve("./project"));
