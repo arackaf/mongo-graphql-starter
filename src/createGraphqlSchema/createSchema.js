8e0b95c107b1b14c83e881684ffe6146f6d6555c
Adam Rackis <arackis@centralreach.com>
2017-09-23 19:39:03 -0500

    More setup and rename the project

--- src/createGraphqlSchema/createSchema.js
+++ src/createGraphqlSchema/createSchema.js
@@ -1,5 +1,9 @@
+import path from "path";
+
 export default function(source, destPath) {
   Promise.resolve(source).then(module => {
     Object.keys(module).forEach(k => console.log(k));
+
+    console.log(path.resolve("project/schema.js", "."));
   });
 }
