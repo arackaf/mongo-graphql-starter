0e7c835948b10307dd032f2b406939907b1149e9
Adam Rackis <adamrackis@gmail.com>
2017-09-23 19:39:03 -0500

    More setup and rename the project

--- test/module.js
+++ test/module.js
@@ -1,3 +1,8 @@
+import path from "path";
+
 import { createGraphqlSchema } from "../index";
+
+//console.log(path.normalize("project/schema.js"));
+console.log(path.resolve("./project/schema.js"));
 
 createGraphqlSchema(import("./project/schema.js"));
