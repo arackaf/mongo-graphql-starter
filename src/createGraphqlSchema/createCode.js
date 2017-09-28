0a5d9da40197c7ea824b2f8ca29cd2d48d14ec4f
Adam Rackis <adamrackis@gmail.com>
2017-09-28 00:23:57 -0500

    schema generation started

--- src/createGraphqlSchema/createCode.js
+++ src/createGraphqlSchema/createCode.js
@@ -1,3 +1,5 @@
+import { MongoId } from "./dataTypes";
+
 const TAB = "  "; //two spaces
 
 export function createObject(declaration, properties, offset = 1) {
@@ -17,12 +19,12 @@ function createProperties(properties, offset) {
     .map(({ name, value }) => {
       return `${propertyTab}${name}: ${Array.isArray(value)
         ? `[\n${nestedObjTab}${createObject("{", value, offset + 2)}\n${propertyTab}]`
-        : displayValue(value)}`;
+        : displayMetadataValue(value)}`;
     })
     .join(",\n");
 }
 
-function displayValue(value) {
+function displayMetadataValue(value) {
   if (typeof value === "string") {
     return `"${value}"`;
   } else if (typeof value === "object") {
@@ -32,6 +34,32 @@ function displayValue(value) {
       return `"${value.type.__name}"`;
     }
   }
+}
+
+function displaySchemaValue(value) {
+  if (typeof value === "string") {
+    return `${value == MongoId ? "String" : value}`;
+  } else if (typeof value === "object") {
+    if (value.__isArray) {
+      return `"[${value.type.__name}]"`;
+    } else if (value.__isObject) {
+      return `"${value.type.__name}"`;
+    }
+  }
+}
+
+export function createGraphqlSchema(objectToCreate) {
+  let fields = objectToCreate.fields;
+
+  return `export default \`
+
+type ${objectToCreate.__name} {
+${TAB}${Object.keys(fields)
+    .map(k => `${k}: ${displaySchemaValue(fields[k])}`)
+    .join(`\n${TAB}`)}
+}
+
+\`;`;
 }
 
 //${properties.map(({ name, value }) => `${propertyTab}${name}: ${value}`).join(",\n")}
