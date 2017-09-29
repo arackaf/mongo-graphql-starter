3b3496775ecdaa34c152b630f8efa4ae656312bf
Adam Rackis <adamrackis@gmail.com>
2017-09-28 23:08:41 -0500

    schemaCreation

--- src/createGraphqlSchema/createCode.js
+++ src/createGraphqlSchema/createCode.js
@@ -1,4 +1,4 @@
-import { MongoId } from "./dataTypes";
+import { MongoId, String, Int, Float, ArrayOf } from "./dataTypes";
 
 const TAB = "  "; //two spaces
 
@@ -48,18 +48,46 @@ function displaySchemaValue(value) {
   }
 }
 
+function queriesForField(fieldName, fieldType) {
+  switch (fieldType) {
+    case String:
+      return [fieldName, `${fieldName}_contains`, `${fieldName}_startsWith`, `${fieldName}_endsWith`].map(p => `${p}: String`);
+    case Int:
+    case Float:
+      return [fieldName, `${fieldName}_lt`, `${fieldName}_lte`, `${fieldName}_gt`, `${fieldName}_gte`].map(
+        p => `${p}: ${fieldType == Int ? "Int" : "Float"}`
+      );
+  }
+  return [];
+}
+
 export function createGraphqlSchema(objectToCreate) {
-  let fields = objectToCreate.fields;
+  let fields = objectToCreate.fields,
+    name = objectToCreate.__name,
+    allFields = [],
+    TAB2 = TAB + TAB;
+
+  Object.keys(fields).forEach(k => {
+    allFields.push(...queriesForField(k, fields[k]));
+  });
+
+  let idField = Object.keys(fields).find(k => fields[k] === MongoId);
 
   return `export default \`
 
-type ${objectToCreate.__name} {
+type ${name} {
 ${TAB}${Object.keys(fields)
     .map(k => `${k}: ${displaySchemaValue(fields[k])}`)
     .join(`\n${TAB}`)}
 }
 
-\`;`;
+type Query {
+${TAB}all${name}s(
+${TAB2}${allFields.join(`,\n${TAB2}`)}
+  ): [${name}]
+
+${idField ? `${TAB}get${name}(${idField}: String): ${name}` : ""}
 }
 
-//${properties.map(({ name, value }) => `${propertyTab}${name}: ${value}`).join(",\n")}
+\`;`;
+}
