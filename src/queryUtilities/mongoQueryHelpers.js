4a1556f571cecc4e7feaeacf6e3fb51310653b46
Adam Rackis <adamrackis@gmail.com>
2017-09-29 18:57:42 -0500

    Move around test setup content, first query filter implemented!

--- src/queryUtilities/mongoQueryHelpers.js
+++ src/queryUtilities/mongoQueryHelpers.js
@@ -1,5 +1,15 @@
 import { parseRequestedFields } from "./parseAst";
+import { MongoId, String, Int, Float } from "../createGraphqlSchema/dataTypes";
 
 export function getMongoProjection(fields) {
   return fields.reduce((hash, field) => ((hash[field] = 1), hash), {});
+}
+
+export function getMongoFilters(args, objectMetaData) {
+  return Object.keys(args).reduce((hash, k) => {
+    if (objectMetaData.fields[k]) {
+      hash[k] = args[k];
+    }
+    return hash;
+  }, {});
 }
--- test/testProject1/testProject1.test.js
+++ test/testProject1/testProject1.test.js
@@ -1,26 +1,3 @@
-import { graphql } from "graphql";
-import { makeExecutableSchema } from "graphql-tools";
-
-import { graphqlAst, createGraphqlSchema } from "../../index";
-const { parseRequestedFields } = graphqlAst;
-
-import { arraysMatch } from "../testUtils";
-import projectSetup from "../projectSetupA";
-
-import path from "path";
-import fs from "fs";
-import del from "del";
-
-let astPassedIn, schema;
-
-beforeAll(() => {
-  try {
-    del.sync("test/testProject1/project");
-  } catch (e) {}
-  fs.mkdirSync("test/testProject1/project");
-  createGraphqlSchema(projectSetup, path.resolve("./test/testProject1/project"));
-});
-
-test("junk", () => {
+test("placeholder", () => {
   expect(1).toBe(1);
 });
