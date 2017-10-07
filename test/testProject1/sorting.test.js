34ffeee70c824b78750a4a07e7ceef0bbaddc120
Adam Rackis <arackis@centralreach.com>
2017-10-07 18:36:12 -0400

    start sorting

---
+++ test/testProject1/sorting.test.js
@@ -0,0 +1,46 @@
+import { MongoClient } from "mongodb";
+import resolvers from "./graphQL/resolver";
+import typeDefs from "./graphQL/schema";
+import { makeExecutableSchema } from "graphql-tools";
+
+import { queryAndMatchArray } from "../testUtil";
+
+let db, schema;
+beforeAll(async () => {
+  db = await MongoClient.connect("mongodb://localhost:27017/mongo-graphql-starter");
+  schema = makeExecutableSchema({ typeDefs, resolvers, initialValue: { db: {} } });
+
+  await db.collection("books").insert({ title: "Book 4", pages: 200 });
+  await db.collection("books").insert({ title: "Book 6", pages: 200 });
+  await db.collection("books").insert({ title: "Book 5", pages: 200 });
+  await db.collection("books").insert({ title: "Book 8", pages: 200 });
+  await db.collection("books").insert({ title: "Book 1", pages: 100 });
+  await db.collection("books").insert({ title: "Book 2", pages: 150 });
+  await db.collection("books").insert({ title: "Book 7", pages: 210 });
+  await db.collection("books").insert({ title: "Book 3", pages: 90 });
+});
+
+afterAll(async () => {
+  await db.collection("books").remove({});
+  db.close();
+  db = null;
+});
+
+test("OR filters 1", async () => {
+  queryAndMatchArray({
+    schema,
+    db,
+    query: "{allBooks(sort: {title: 1}){title, pages}}",
+    coll: "allBooks",
+    results: [
+      { title: "Book 1", pages: 100 },
+      { title: "Book 2", pages: 150 },
+      { title: "Book 3", pages: 90 },
+      { title: "Book 4", pages: 200 },
+      { title: "Book 5", pages: 200 },
+      { title: "Book 6", pages: 200 },
+      { title: "Book 7", pages: 210 },
+      { title: "Book 8", pages: 200 }
+    ]
+  });
+});
