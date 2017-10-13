8aaaa69b126a7db92f74eb201d6f2b021b9432c9
Adam Rackis <arackis@centralreach.com>
2017-10-13 13:55:57 -0500

    Sorting works

--- src/queryUtilities/decontructGraphqlQuery.js
+++ src/queryUtilities/decontructGraphqlQuery.js
@@ -5,7 +5,18 @@ export default function(args, ast, objectMetaData) {
   let $match = getMongoFilters(args, objectMetaData),
     { primitives: requestedFields } = parseRequestedFields(ast),
     $project = getMongoProjection(requestedFields),
-    $sort = args.SORT;
+    sort = args.SORT,
+    sorts = args.SORTS,
+    $sort;
+
+  if (sort) {
+    $sort = sort;
+  } else if (sorts) {
+    $sort = {};
+    sorts.forEach(packet => {
+      Object.assign($sort, packet);
+    });
+  }
 
   return { $match, requestedFields, $project, $sort };
 }
