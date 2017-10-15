e13c8c9be4c677c337eb2ab22c3834592593c41e
Adam Rackis <adamrackis@gmail.com>
2017-10-15 16:03:20 -0500

    query non-primitive types

--- test/testProject1/readNonPrimitiveTypes.test.js
+++ test/testProject1/readNonPrimitiveTypes.test.js
@@ -10,7 +10,7 @@ beforeAll(async () => {
   db = await MongoClient.connect("mongodb://localhost:27017/mongo-graphql-starter");
   schema = makeExecutableSchema({ typeDefs, resolvers, initialValue: { db: {} } });
 
-  await db.collection("books").insert({ title: "Book 100", pages: 100, authors: [{ _id: "1", name: "Adam" }] });
+  await db.collection("books").insert({ title: "Book 100", pages: 100, authors: [{ _id: "1", name: "Adam" }], strArrs: [["a"], ["b", "c"]] });
   await db.collection("books").insert({ title: "Book 150", pages: 150, authors: [{ _id: "2", name: "Bob" }] });
   await db.collection("books").insert({ title: "Book 200", pages: 200, authors: [{ _id: "1", name: "Adam" }, { _id: 2, name: "Bob" }] });
 });
@@ -28,5 +28,25 @@ test("Fetches authors", async () => {
     query: "{allBooks(pages: 100){title, authors { _id, name }}}",
     coll: "allBooks",
     results: [{ title: "Book 100", authors: [{ _id: "1", name: "Adam" }] }]
+  });
+});
+
+test("Fetches strArrays", async () => {
+  await queryAndMatchArray({
+    schema,
+    db,
+    query: "{allBooks(pages: 100){title, strArrs}}",
+    coll: "allBooks",
+    results: [{ title: "Book 100", strArrs: [["a"], ["b", "c"]] }]
+  });
+});
+
+test("Fetches both", async () => {
+  await queryAndMatchArray({
+    schema,
+    db,
+    query: "{allBooks(pages: 100){title, strArrs, authors { _id, name }}}",
+    coll: "allBooks",
+    results: [{ title: "Book 100", authors: [{ _id: "1", name: "Adam" }], strArrs: [["a"], ["b", "c"]] }]
   });
 });
