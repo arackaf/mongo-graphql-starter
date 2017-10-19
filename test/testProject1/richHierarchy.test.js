c6a85db745913454c72400a6a99743957b1ac271
Adam Rackis <adamrackis@hotmail.com>
2017-10-18 22:09:14 -0500

    expand tests

--- test/testProject1/richHierarchy.test.js
+++ test/testProject1/richHierarchy.test.js
@@ -101,136 +101,44 @@ test("Add reviewers with tags to author", async () => {
   });
 });
 
-// test("Creation mutation runs and returns object, then searched with graphQL. Check non-created fields", async () => {
-//   let obj = await runMutation({ schema, db, mutation: `createBook(title: "Book 3", pages: 150){_id}`, result: "createBook" });
-//   await queryAndMatchArray({
-//     schema,
-//     db,
-//     query: `{getBook(_id: "${obj._id}"){title, pages, weight}}`,
-//     coll: "getBook",
-//     results: { title: "Book 3", pages: 150, weight: null }
-//   });
-// });
-
-// test("Creation mutation runs and returns object with formatting", async () => {
-//   let obj = await runMutation({
-//     schema,
-//     db,
-//     mutation: `createBook(title: "Book 2", pages: 100, weight: 1.2, authors: [{birthday: "1982-03-22", name: "Adam"}, {birthday: "2004-06-02", name: "Bob"}], primaryAuthor: {birthday: "2004-06-02", name: "Bob"}, strArrs: [["a"], ["b", "c"]], createdOn: "2004-06-03", createdOnYearOnly: "2004-06-03"){title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "createBook"
-//   });
-//   expect(obj).toEqual({
-//     title: "Book 2",
-//     pages: 100,
-//     weight: 1.2,
-//     authors: [{ birthday: "03/22/1982", name: "Adam" }, { birthday: "06/02/2004", name: "Bob" }],
-//     primaryAuthor: { birthday: "06/02/2004", name: "Bob" },
-//     strArrs: [["a"], ["b", "c"]],
-//     createdOn: "06/03/2004",
-//     createdOnYearOnly: "2004"
-//   });
-// });
-
-// test("Modification mutation works", async () => {
-//   let obj = await runMutation({
-//     schema,
-//     db,
-//     mutation: `createBook(title: "Book 2", pages: 100, weight: 1.2, authors: [{birthday: "1982-03-22", name: "Adam"}, {birthday: "2004-06-02", name: "Bob"}], primaryAuthor: {birthday: "2004-06-02", name: "Bob"}, strArrs: [["a"], ["b", "c"]], createdOn: "2004-06-03", createdOnYearOnly: "2004-06-03"){_id, title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "createBook"
-//   });
-
-//   let updated = await runMutation({
-//     schema,
-//     db,
-//     mutation: `updateBook(_id: "${obj._id}", title: "Book 2a", pages: 101, weight: 1.3, authors: [{birthday: "1982-03-23", name: "Adam R"}, {birthday: "2004-06-03", name: "Bob B"}], primaryAuthor: {birthday: "2000-01-02", name: "Mike"}, strArrs: [["d"], ["e", "f"]], createdOn: "2004-06-04", createdOnYearOnly: "2004-06-05"){title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "updateBook"
-//   });
-//   expect(updated).toEqual({
-//     title: "Book 2a",
-//     pages: 101,
-//     weight: 1.3,
-//     authors: [{ birthday: "03/23/1982", name: "Adam R" }, { birthday: "06/03/2004", name: "Bob B" }],
-//     primaryAuthor: { birthday: "01/02/2000", name: "Mike" },
-//     strArrs: [["d"], ["e", "f"]],
-//     createdOn: "06/04/2004",
-//     createdOnYearOnly: "2004"
-//   });
-// });
-
-// test("Modification mutation works", async () => {
-//   let obj = await runMutation({
-//     schema,
-//     db,
-//     mutation: `createBook(authors: [{birthday: "1982-03-22", name: "Adam"}], primaryAuthor: {birthday: "2004-06-02", name: "Bob"}){_id, title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "createBook"
-//   });
-
-//   let updated = await runMutation({
-//     schema,
-//     db,
-//     mutation: `updateBook(_id: "${obj._id}", authors: [{birthday: "1982-03-23", name: "Adam R"}, {birthday: "2004-06-03", name: "Bob B"}], primaryAuthor: {birthday: "2000-01-02", name: "Mike"}){title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "updateBook"
-//   });
-//   expect(updated).toEqual({
-//     title: null,
-//     pages: null,
-//     weight: null,
-//     authors: [{ birthday: "03/23/1982", name: "Adam R" }, { birthday: "06/03/2004", name: "Bob B" }],
-//     primaryAuthor: { birthday: "01/02/2000", name: "Mike" },
-//     strArrs: null,
-//     createdOn: null,
-//     createdOnYearOnly: null
-//   });
-// });
-
-// test("Partial modification mutation works", async () => {
-//   let obj = await runMutation({
-//     schema,
-//     db,
-//     mutation: `createBook(title: "Book 2", pages: 100, weight: 1.2, authors: [{birthday: "1982-03-22", name: "Adam"}, {birthday: "2004-06-02", name: "Bob"}], primaryAuthor: {birthday: "2004-06-02", name: "Bob"}, strArrs: [["a"], ["b", "c"]], createdOn: "2004-06-03", createdOnYearOnly: "2004-06-03"){_id, title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "createBook"
-//   });
-
-//   let updated = await runMutation({
-//     schema,
-//     db,
-//     mutation: `updateBook(_id: "${obj._id}", title: "Book 2a", pages: 101){title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "updateBook"
-//   });
-//   expect(updated).toEqual({
-//     title: "Book 2a",
-//     pages: 101,
-//     weight: 1.2,
-//     authors: [{ birthday: "03/22/1982", name: "Adam" }, { birthday: "06/02/2004", name: "Bob" }],
-//     primaryAuthor: { birthday: "06/02/2004", name: "Bob" },
-//     strArrs: [["a"], ["b", "c"]],
-//     createdOn: "06/03/2004",
-//     createdOnYearOnly: "2004"
-//   });
-// });
-
-// test("No modification mutation works", async () => {
-//   let obj = await runMutation({
-//     schema,
-//     db,
-//     mutation: `createBook(title: "Book 2", pages: 100, weight: 1.2, authors: [{birthday: "1982-03-22", name: "Adam"}, {birthday: "2004-06-02", name: "Bob"}], primaryAuthor: {birthday: "2004-06-02", name: "Bob"}, strArrs: [["a"], ["b", "c"]], createdOn: "2004-06-03", createdOnYearOnly: "2004-06-03"){_id, title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "createBook"
-//   });
-
-//   let updated = await runMutation({
-//     schema,
-//     db,
-//     mutation: `updateBook(_id: "${obj._id}"){title, pages, weight, authors { birthday, name }, primaryAuthor{ birthday, name }, strArrs, createdOn, createdOnYearOnly}`,
-//     result: "updateBook"
-//   });
-//   expect(updated).toEqual({
-//     title: "Book 2",
-//     pages: 100,
-//     weight: 1.2,
-//     authors: [{ birthday: "03/22/1982", name: "Adam" }, { birthday: "06/02/2004", name: "Bob" }],
-//     primaryAuthor: { birthday: "06/02/2004", name: "Bob" },
-//     strArrs: [["a"], ["b", "c"]],
-//     createdOn: "06/03/2004",
-//     createdOnYearOnly: "2004"
-//   });
-// });
+test("Add favorite tag to author and reviewers reviewers with tags to author", async () => {
+  let obj = await runMutation({
+    schema,
+    db,
+    mutation: `
+      createBlog(
+        title: "Blog 1", 
+        content: "Hello", 
+        author: { name: "Adam Auth", birthday: "2004-06-02", favoriteTag: {name: "tf"}, tagsSubscribed: [{name: "t1"}, {name: "t2"}]},
+        comments: [{
+          text: "C1", 
+          reviewers: [{
+            name: "Adam", 
+            birthday: "1982-03-22", 
+            tagsSubscribed: [{name: "t1"}, {name: "t2"}]
+          }, {
+            name: "Adam2", 
+            birthday: "1982-03-23", 
+            tagsSubscribed: [{name: "t3"}, {name: "t4"}]
+          }],
+          author: { name: "Adam", birthday: "1982-03-22", favoriteTag: {name: "tf"}, tagsSubscribed: [{name: "t1"}, {name: "t2"}]} 
+        }]
+      ){title, content, author{name, birthday, favoriteTag{name}, tagsSubscribed{name}}, comments{text, reviewers{name, birthday, tagsSubscribed{name}}, author{name, birthday, favoriteTag{name}, tagsSubscribed{name}}}}`,
+    result: "createBlog"
+  });
+  expect(obj).toEqual({
+    title: "Blog 1",
+    content: "Hello",
+    author: { name: "Adam Auth", birthday: "06/02/2004", favoriteTag: { name: "tf" }, tagsSubscribed: [{ name: "t1" }, { name: "t2" }] },
+    comments: [
+      {
+        text: "C1",
+        reviewers: [
+          { name: "Adam", birthday: "03/22/1982", tagsSubscribed: [{ name: "t1" }, { name: "t2" }] },
+          { name: "Adam2", birthday: "03/23/1982", tagsSubscribed: [{ name: "t3" }, { name: "t4" }] }
+        ],
+        author: { name: "Adam", birthday: "03/22/1982", favoriteTag: { name: "tf" }, tagsSubscribed: [{ name: "t1" }, { name: "t2" }] }
+      }
+    ]
+  });
+});
