f68f36ff6842b609f7fdd339a75b01f5209ac4c2
Adam Rackis <adamrackis@gmail.com>
2017-10-19 22:18:46 -0500

    Update docs

---
+++ exampleGeneratedCode/bookProject/Author/schema.js
@@ -0,0 +1,22 @@
+export const type = `
+
+type Author {
+  name: String
+  birthday: String
+}
+
+input AuthorInput {
+  name: String
+  birthday: String
+}
+
+input AuthorSort {
+  name: Int
+  birthday: Int
+}
+
+
+
+`;
+
+
