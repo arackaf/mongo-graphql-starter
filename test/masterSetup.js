fa3df258ac6f0e0e9d235da30b471e03b15b54fb
Adam Rackis <adamrackis@hotmail.com>
2017-10-21 16:43:53 -0500

    circular dependencies work

--- test/masterSetup.js
+++ test/masterSetup.js
@@ -3,6 +3,7 @@ const del = require("del");
 try {
   del.sync("test/testProject1/graphQL");
   del.sync("test/testProject2/graphQL");
+  del.sync("test/testProject3/graphQL");
 } catch (e) {}
 
 require = require("@std/esm")(module, { esm: "js", cjs: true });
