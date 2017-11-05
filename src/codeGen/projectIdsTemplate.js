a16b1dce5a422219c40baec9fc952ce76d6aa6a0
Adam Rackis <adamrackis@gmail.com>
2017-11-04 22:27:44 -0500

    reading nested data!

--- src/codeGen/projectIdsTemplate.js
+++ src/codeGen/projectIdsTemplate.js
@@ -1,10 +1,10 @@
-  if (${sourceObjName}s.length && queryPacket.extrasPackets.${targetObjName}){
+  if (${sourceObjName}s.length && queryPacket.extrasPackets.has("${targetObjName}")) {
     let $match = { _id: { $in: flatMap(${sourceObjName}s.map(${objNameLower} => ${objNameLower}.${fkField}), ids => ids.map(id => ObjectId(id))) } };  
     let $project = {};
 
     let results = await load${targetTypeName}s(db, { 
       $match,
-      $project: queryPacket.extrasPackets.${targetObjName}.$project
+      $project: queryPacket.extrasPackets.get("${targetObjName}").$project
     });
 
     let ${targetTypeNameLower}DestinationMap = new Map([]);
@@ -16,14 +16,14 @@
           ${targetTypeNameLower}DestinationMap.set("" + _id, []);
         }
         ${targetTypeNameLower}DestinationMap.get("" + _id).push(${objNameLower}.${targetObjName});
-      });
-    });
+      }
+    }
 
     for (let ${targetTypeNameLower} of results) {
-      if (${targetTypeNameLower}DestinationMap.has("" + ${targetTypeNameLower}._id)){
-        for (let targetArr of ${targetTypeNameLower}DestinationMap.get("" + ${targetTypeNameLower}._id)){
+      if (${targetTypeNameLower}DestinationMap.has("" + ${targetTypeNameLower}._id)) {
+        for (let targetArr of ${targetTypeNameLower}DestinationMap.get("" + ${targetTypeNameLower}._id)) {
           targetArr.push(${targetTypeNameLower});
         }
       }
-    })
+    }
   }
