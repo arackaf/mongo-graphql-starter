97f54cf616b4d6b78244356f409ae31eb15ccb32
Adam Rackis <adamrackis@gmail.com>
2017-11-19 18:19:32 -0600

    add first book relationship

--- test/testProject4/readAuthors.test.js
+++ test/testProject4/readAuthors.test.js
@@ -1,4 +1,5 @@
 import spinUp from "./spinUp";
+import { ObjectId } from "mongodb";
 
 let db, schema, queryAndMatchArray, runMutation;
 beforeAll(async () => {
@@ -11,9 +12,15 @@ beforeAll(async () => {
 
   await Promise.all([adam, katie, laura, mallory].map(person => db.collection("authors").insert(person)));
 
-  await db.collection("books").insert({ title: "Book 1", pages: 100, authorIds: ["" + adam._id] });
-  await db.collection("books").insert({ title: "Book 2", pages: 150, authorIds: ["" + adam._id] });
-  await db.collection("books").insert({ title: "Book 3", pages: 200, authorIds: ["" + katie._id] });
+  let book1 = { title: "Book 1", pages: 100, authorIds: ["" + adam._id] };
+  let book2 = { title: "Book 2", pages: 150, authorIds: ["" + adam._id] };
+  let book3 = { title: "Book 3", pages: 200, authorIds: ["" + katie._id] };
+
+  await db.collection("books").insert(book1);
+  await db.collection("books").insert(book2);
+  await db.collection("books").insert(book3);
+
+  await db.collection("authors").update({ _id: ObjectId(adam._id) }, { $set: { firstBookId: "" + book2._id } });
 });
 
 afterAll(async () => {
@@ -31,6 +38,18 @@ test("Read authors", async () => {
       { title: "Book 1", authors: [{ name: "Adam" }] },
       { title: "Book 2", authors: [{ name: "Adam" }] },
       { title: "Book 3", authors: [{ name: "Katie" }] }
+    ]
+  });
+});
+
+test("Read authors' first book", async () => {
+  await queryAndMatchArray({
+    query: `{allBooks(title_startsWith: "B"){Books{title, authors{name, firstBook{title}}}}}`,
+    coll: "allBooks",
+    results: [
+      { title: "Book 1", authors: [{ name: "Adam", firstBook: { title: "Book 2" } }] },
+      { title: "Book 2", authors: [{ name: "Adam", firstBook: { title: "Book 2" } }] },
+      { title: "Book 3", authors: [{ name: "Katie", firstBook: null }] }
     ]
   });
 });
