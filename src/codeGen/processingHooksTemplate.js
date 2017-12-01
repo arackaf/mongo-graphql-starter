d284b195624ba353571513ef59ea2a591eb724ce
Adam Rackis <adamrackis@gmail.com>
2017-11-30 21:24:05 -0600

    more test updates and setup

--- src/codeGen/processingHooksTemplate.js
+++ src/codeGen/processingHooksTemplate.js
@@ -1,8 +1,17 @@
 export default {
   queryPreprocess(root, args, context, ast) {
-    //
+    //this will be called
   },
   queryMiddleware(queryPacket, root, args, context, ast) {
-    //
+    //this will be called
+  },
+  beforeMutate(filter, updates, root, args, context, ast) {
+    //this will be called
+  },
+  beforeMutate(filter, updates, root, args, context, ast) {
+    //this will be called
+  },
+  beforeInsert(obj, root, args, context, ast) {
+    //this will be called
   }
 };
--- src/module.js
+++ src/module.js
@@ -9,3 +9,12 @@ export { dataTypes };
 
 import * as queryUtilities from "./queryUtilities";
 export { queryUtilities };
+
+export async function processHook(hooks, TypeName, hookName, ...args) {
+  if (hooks[hookName]) {
+    await hooks[hookName](...args);
+  }
+  if (hooks[TypeName] && hooks[TypeName][hookName]) {
+    await hooks[TypeName][hookName](...args);
+  }
+}
