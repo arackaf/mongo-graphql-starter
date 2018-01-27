f2255a3a8600ac8081124f7cd292fe02bc3ef6b5
Adam Rackis <adamrackis@hotmail.com>
2018-01-27 09:21:27 -0600

    better error messages in hooks

--- src/module.js
+++ src/module.js
@@ -16,25 +16,33 @@ export async function processHook(hooks, TypeName, hookName, ...args) {
   let rootHooks = hooks.Root;
   let typeHooks = hooks[TypeName];
   if (rootHooks) {
-    if (typeof rootHooks === "function") {
-      rootHooks = new rootHooks();
-    }
-    if (rootHooks[hookName]) {
-      let res = await rootHooks[hookName](...args);
-      if (shortCircuitHooks.has(hookName) && res === false) {
-        return false;
+    try {
+      if (typeof rootHooks === "function") {
+        rootHooks = new rootHooks();
       }
+      if (rootHooks[hookName]) {
+        let res = await rootHooks[hookName](...args);
+        if (shortCircuitHooks.has(hookName) && res === false) {
+          return false;
+        }
+      }
+    } catch (err) {
+      throw `The following error happened in root hook method ${hookName}: ${err.toString()}`;
     }
   }
   if (typeHooks) {
-    if (typeof typeHooks === "function") {
-      typeHooks = new typeHooks();
-    }
-    if (typeHooks[hookName]) {
-      let res = await typeHooks[hookName](...args);
-      if (shortCircuitHooks.has(hookName) && res === false) {
-        return false;
+    try {
+      if (typeof typeHooks === "function") {
+        typeHooks = new typeHooks();
       }
+      if (typeHooks[hookName]) {
+        let res = await typeHooks[hookName](...args);
+        if (shortCircuitHooks.has(hookName) && res === false) {
+          return false;
+        }
+      }
+    } catch (err) {
+      throw `The following error happened in hook method ${hookName} for type ${TypeName}: ${err.toString()}`;
     }
   }
 }
