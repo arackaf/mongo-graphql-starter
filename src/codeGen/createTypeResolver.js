1c1061e0e4ac72c5ae34c3ce3d8425523005ed2f
Adam Rackis <adamrackis@gmail.com>
2018-02-08 01:02:13 -0500

    start setting up context object

--- src/codeGen/createTypeResolver.js
+++ src/codeGen/createTypeResolver.js
@@ -5,6 +5,7 @@ export default function createGraphqlResolver(objectToCreate) {
   let template = fs.readFileSync(path.resolve(__dirname, "./resolverTemplate.js"), { encoding: "utf8" });
   let projectIdsTemplate = fs.readFileSync(path.resolve(__dirname, "./projectIdsTemplate.js"), { encoding: "utf8" });
   let projectIdTemplate = fs.readFileSync(path.resolve(__dirname, "./projectIdTemplate.js"), { encoding: "utf8" });
+  let projectIdResolverTemplate = fs.readFileSync(path.resolve(__dirname, "./projectIdResolverTemplate.js"), { encoding: "utf8" });
   let result = "";
   let imports = [
     `import { queryUtilities, processHook } from "mongo-graphql-starter";`,
@@ -16,6 +17,7 @@ export default function createGraphqlResolver(objectToCreate) {
   ];
   let typeImports = new Set([]);
   let relationships = "";
+  let relationshipResolvers = "";
 
   if (objectToCreate.relationships) {
     Object.keys(objectToCreate.relationships).forEach((relationshipName, index, all) => {
@@ -29,6 +31,11 @@ export default function createGraphqlResolver(objectToCreate) {
       if (!typeImports.has("flatMap")) {
         typeImports.add("flatMap");
         imports.push(`import flatMap from "lodash.flatmap";`);
+      }
+
+      if (!typeImports.has("dataloader")) {
+        typeImports.add("dataloader");
+        imports.push(`import DataLoader from "dataloader";`);
       }
 
       if (relationship.__isArray) {
@@ -49,6 +56,16 @@ export default function createGraphqlResolver(objectToCreate) {
           .replace(/\${targetTypeNameLower}/g, relationship.type.__name.toLowerCase())
           .replace(/\${sourceParam}/g, objectToCreate.__name.toLowerCase())
           .replace(/\${sourceObjName}/g, objectToCreate.__name);
+
+        relationshipResolvers += projectIdResolverTemplate
+          .replace(/\${table}/g, relationship.type.table)
+          .replace(/\${fkField}/g, relationship.fkField)
+          .replace(/\${targetObjName}/g, relationshipName)
+          .replace(/\${targetTypeName}/g, relationship.type.__name)
+          .replace(/\${targetTypeNameLower}/g, relationship.type.__name.toLowerCase())
+          .replace(/\${sourceParam}/g, objectToCreate.__name.toLowerCase())
+          .replace(/\${sourceObjName}/g, objectToCreate.__name)
+          .replace(/\${dataLoaderId}/g, `__${objectToCreate.__name}_${relationshipName}DataLoader`);
       }
 
       if (index < all.length - 1) {
@@ -61,6 +78,7 @@ export default function createGraphqlResolver(objectToCreate) {
 
   result += template
     .replace(/\${relationships}/g, relationships)
+    .replace(/\${relationshipResolvers}/g, relationshipResolvers)
     .replace(/\${table}/g, objectToCreate.table)
     .replace(/\${objName}/g, objectToCreate.__name)
     .replace(/\${objNameLower}/g, objectToCreate.__name.toLowerCase());
