d18106b5de86d1059dbfa22217b1a1f422041e71
Adam Rackis <adamrackis@hotmail.com>
2018-04-15 19:29:23 -0500

    docs updated, single test added

--- test/testProject5/addAuthorsToNewBooks.test.js
+++ test/testProject5/addAuthorsToNewBooks.test.js
@@ -38,6 +38,25 @@ test("Add single new new author in new book", async () => {
   });
 });
 
+test("Add mainAuthor in new book", async () => {
+  let newBook = await runMutation({
+    mutation: `createBook(Book: {title: "New Book", mainAuthor: { name: "New Author" }}){Book{_id, title, mainAuthor{name}}}`,
+    result: "createBook"
+  });
+
+  await queryAndMatchArray({
+    query: `{allBooks(_id_in: ["${newBook._id}"]){Books{title, mainAuthor{name}}}}`,
+    coll: "allBooks",
+    results: [{ title: "New Book", mainAuthor: { name: "New Author" } }]
+  });
+
+  await queryAndMatchArray({
+    query: `{allAuthors(name: "New Author"){Authors{name}}}`,
+    coll: "allAuthors",
+    results: [{ name: "New Author" }] //just one
+  });
+});
+
 test("Add two new new authors in new book", async () => {
   let newBook = await runMutation({
     mutation: `createBook(Book: {title: "New Book", authors: [{ name: "New Author 1" }, { name: "New Author 2" }]}){Book{_id, title, authors{name}}}`,
