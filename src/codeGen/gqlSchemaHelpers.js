5529925b1012fe12bf40b403f8317c9312792462
Adam Rackis <adamrackis@gmail.com>
2018-04-20 00:16:05 -0500

    start to clean up input type generation

--- src/codeGen/gqlSchemaHelpers.js
+++ src/codeGen/gqlSchemaHelpers.js
@@ -5,3 +5,11 @@ export function createOperation({ overrides }, name, args, returnType) {
   if (overrides.has(name)) return "";
   return `${TAB}${name} (\n${TAB2}${args.join(`,\n${TAB2}`)}\n${TAB}): ${returnType}`;
 }
+
+export function createInput(name, fields) {
+  return createGenericType("input", name, fields);
+}
+
+function createGenericType(typeName, name, fields) {
+  return `${TAB}${typeName} ${name} {\n${TAB2}${fields.map(([name, val]) => `${name}: ${val}`).join(`\n${TAB2}`)}\n${TAB}}`;
+}
