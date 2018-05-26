a1e32643782629134e0996aa547cfc9d925f9e1a
Adam Rackis <adamrackis@gmail.com>
2018-05-26 00:15:29 -0500

    projects 2 and 3

--- test/testProject2/spinUp.js
+++ test/testProject2/spinUp.js
@@ -1,10 +1,20 @@
 import { MongoClient } from "mongodb";
 import { queryAndMatchArray, runMutation, nextConnectionString } from "../testUtil";
-import resolvers from "./graphQL/resolver";
-import typeDefs from "./graphQL/schema";
 import { makeExecutableSchema } from "graphql-tools";
+import { createGraphqlSchema } from "../../src/module";
+import path from "path";
+
+import projectSetupB from "../projectSetupB";
+
+export async function create() {
+  return createGraphqlSchema(projectSetupB, path.resolve("./test/testProject2"));
+}
 
 export default async function() {
+  await create();
+
+  const [{ default: resolvers }, { default: typeDefs }] = await Promise.all([import("./graphQL/resolver"), import("./graphQL/schema")]);
+
   let db, schema;
   db = await MongoClient.connect(nextConnectionString());
   schema = makeExecutableSchema({ typeDefs, resolvers, initialValue: { db: {} } });
