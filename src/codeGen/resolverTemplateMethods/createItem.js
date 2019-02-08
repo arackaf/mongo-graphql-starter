b48db0a46e567632523a8594d284fa37a50244e5
Adam.Rackis <adam.rackis@riotgames.com>
2019-02-08 10:53:35 -0600

    code generation cleanup

--- src/codeGen/resolverTemplateMethods/createItem.js
+++ src/codeGen/resolverTemplateMethods/createItem.js
@@ -4,15 +4,15 @@ export default ({ objName }) =>
   `    async create${objName}(root, args, context, ast) {
       ${mutationStart({ objName, op: "create" })}
       try {
-        let newObject = await newObjectFromArgs(args.${objName}, ${objName}Metadata, { ...gqlPacket, db, dbHelpers, hooksObj, session });
+        let newObject = await newObjectFromArgs(args.${objName}, ${objName}Metadata, { ...gqlPacket, db, session });
         let requestMap = parseRequestedFields(ast, "${objName}");
         let $project = requestMap.size ? getMongoProjection(requestMap, ${objName}Metadata, args) : null;
 
-        newObject = await dbHelpers.processInsertion(db, newObject, { ...gqlPacket, typeMetadata: ${objName}Metadata, hooksObj, session });
+        newObject = await dbHelpers.processInsertion(db, newObject, { ...gqlPacket, typeMetadata: ${objName}Metadata, session });
         if (newObject == null) {
           return { ${objName}: null };
         }
-        await setUpOneToManyRelationships(newObject, args.${objName}, ${objName}Metadata, { ...gqlPacket, db, hooksObj, session });
+        await setUpOneToManyRelationships(newObject, args.${objName}, ${objName}Metadata, { ...gqlPacket, db, session });
         ${mutationComplete()}
 
         let result = $project ? (await load${objName}s(db, { $match: { _id: newObject._id }, $project, $limit: 1 }, root, args, context, ast))[0] : null;
--- src/codeGen/resolverTemplateMethods/updateItems.js
+++ src/codeGen/resolverTemplateMethods/updateItems.js
@@ -4,14 +4,14 @@ export default ({ objName, table }) => `    async update${objName}s(root, args, 
       ${mutationStart({ objName, op: "update" })}
       try {
         let { $match, $project } = decontructGraphqlQuery({ _id_in: args._ids }, ast, ${objName}Metadata, "${objName}s");
-        let updates = await getUpdateObject(args.Updates || {}, ${objName}Metadata, { ...gqlPacket, db, dbHelpers, hooksObj, session });
+        let updates = await getUpdateObject(args.Updates || {}, ${objName}Metadata, { ...gqlPacket, db, session });
 
-        if (await processHook(hooksObj, "${objName}", "beforeUpdate", $match, updates, { ...gqlPacket, db, session }) === false) {
+        if (await runHook("beforeUpdate", $match, updates, { ...gqlPacket, db, session }) === false) {
           return { success: true };
         }
-        await setUpOneToManyRelationshipsForUpdate(args._ids, args, ${objName}Metadata, { ...gqlPacket, db, dbHelpers, hooksObj, session });
+        await setUpOneToManyRelationshipsForUpdate(args._ids, args, ${objName}Metadata, { ...gqlPacket, db, session });
         await dbHelpers.runUpdate(db, "${table}", $match, updates, { session, multi: true });
-        await processHook(hooksObj, "${objName}", "afterUpdate", $match, updates, { ...gqlPacket, db, session });
+        await runHook("afterUpdate", $match, updates, { ...gqlPacket, db, session });
         ${mutationComplete()}
         
         let result = $project ? await load${objName}s(db, { $match, $project }, root, args, context, ast) : null;
