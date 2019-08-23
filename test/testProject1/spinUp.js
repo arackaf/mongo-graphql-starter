58e1ae1f7383a0c0147c81f8bf859ae006b408e7
Adam.Rackis <adam.rackis@riotgames.com>
2019-08-23 10:13:18 -0500

    Update Babel, Jest, and Mongo connection code

--- test/testProject1/spinUp.js
+++ test/testProject1/spinUp.js
@@ -25,7 +25,7 @@ export default async function() {
   const [{ default: resolvers }, { default: typeDefs }] = await Promise.all([import("./graphQL/resolver"), import("./graphQL/schema")]);
 
   let db, schema;
-  let client = await MongoClient.connect(nextConnectionString(), { useNewUrlParser: true });
+  let client = await MongoClient.connect(nextConnectionString(), { useNewUrlParser: true, useUnifiedTopology: true });
   db = client.db(process.env.databaseName || "mongo-graphql-starter");
   schema = makeExecutableSchema({ typeDefs, resolvers, initialValue: { db: {} } });
 
