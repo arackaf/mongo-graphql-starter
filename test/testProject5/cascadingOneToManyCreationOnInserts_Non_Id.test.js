85594f55343edac6b4fcbfdd91ac6d4b8041f4e7
Duncan Healy <duncan.healy@gmail.com>
2020-05-19 17:07:00 +0100

    delete records before running for failing tests

--- test/testProject5/cascadingOneToManyCreationOnInserts_Non_Id.test.js
+++ test/testProject5/cascadingOneToManyCreationOnInserts_Non_Id.test.js
@@ -6,6 +6,10 @@ let adam, katie, laura, mallory, book1, book2, book3;
 
 beforeAll(async () => {
   ({ db, schema, queryAndMatchArray, runQuery, runMutation, close } = await spinUp());
+  await db.collection("books").deleteMany({});
+  await db.collection("authors").deleteMany({});
+  await db.collection("subjects").deleteMany({});
+  await db.collection("keywords").deleteMany({});
 });
 
 afterEach(async () => {
@@ -107,7 +111,10 @@ test("Add authorNamesBooks entry in new author, and nested objects B", async () 
             mainAuthor: {
               name: "ma",
               mainSubject: { name: "ms" },
-              subjects: [{ name: "s1", keywords: [] }, { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }]
+              subjects: [
+                { name: "s1", keywords: [] },
+                { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }
+              ]
             }
           }
         ]
@@ -160,7 +167,10 @@ test("Add authorNamesBooks entry in new author, and nested objects C", async () 
               name: "ma",
               mainSubject: { name: "ms" },
               authorNamesBooks: [{ title: "Nested Book 1" }],
-              subjects: [{ name: "s1", keywords: [] }, { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }]
+              subjects: [
+                { name: "s1", keywords: [] },
+                { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }
+              ]
             }
           }
         ]
@@ -171,7 +181,10 @@ test("Add authorNamesBooks entry in new author, and nested objects C", async () 
   await queryAndMatchArray({
     query: `{allBooks(SORT: {title: 1}){Books{title authorNames}}}`,
     coll: "allBooks",
-    results: [{ title: "Nested Book 1", authorNames: ["ma"] }, { title: "New Book 1", authorNames: ["adam"] }]
+    results: [
+      { title: "Nested Book 1", authorNames: ["ma"] },
+      { title: "New Book 1", authorNames: ["adam"] }
+    ]
   });
 
   await queryAndMatchArray({
@@ -221,7 +234,10 @@ test("Add authorNamesBooks entry in new author, and nested objects D", async () 
               name: "ma",
               mainSubject: { name: "ms" },
               authorNamesBooks: [{ title: "Nested Book B" }],
-              subjects: [{ name: "s1", keywords: [] }, { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }]
+              subjects: [
+                { name: "s1", keywords: [] },
+                { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }
+              ]
             }
           }
         ]
@@ -311,7 +327,10 @@ test("Add books entry in new author, and nested objects E", async () => {
 
   const A1_BOOKS = [{ title: "Nested Book A1" }, { title: "New Book 1" }];
   const A2_BOOKS = [{ title: "B1" }, { title: "Nested Book A1" }];
-  const NEW_BOOK1_AUTHORS = [{ name: "A1", authorNamesBooks: A1_BOOKS }, { name: "Adam", authorNamesBooks: [{ title: "New Book 1" }] }];
+  const NEW_BOOK1_AUTHORS = [
+    { name: "A1", authorNamesBooks: A1_BOOKS },
+    { name: "Adam", authorNamesBooks: [{ title: "New Book 1" }] }
+  ];
 
   await queryAndMatchArray({
     query: `{allAuthors(name: "Adam"){Authors{name, authorNamesBooks{title, authorsByName(SORT: {name: 1}){name, authorNamesBooks(SORT: {title: 1}){title, authorsByName(SORT: {name: 1}){name, authorNamesBooks(SORT: {title: 1}){title}}}}, mainAuthor {name, mainSubject{name}, authorNamesBooks(SORT: {title: 1}){title, authorsByName(SORT: {name: 1}){name, authorNamesBooks(SORT: {title: 1}){title}}}, subjects(SORT: {name: 1}){name, keywords(SORT: {keywordName: 1}){keywordName}}} }}}}`,
@@ -328,7 +347,10 @@ test("Add books entry in new author, and nested objects E", async () => {
                 authorNamesBooks: [
                   {
                     title: "Nested Book A1",
-                    authorsByName: [{ name: "A1", authorNamesBooks: A1_BOOKS }, { name: "A2", authorNamesBooks: A2_BOOKS }]
+                    authorsByName: [
+                      { name: "A1", authorNamesBooks: A1_BOOKS },
+                      { name: "A2", authorNamesBooks: A2_BOOKS }
+                    ]
                   },
                   {
                     title: "New Book 1",
@@ -358,7 +380,10 @@ test("Add books entry in new author, and nested objects E", async () => {
                   ]
                 }
               ],
-              subjects: [{ name: "s1", keywords: [] }, { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }]
+              subjects: [
+                { name: "s1", keywords: [] },
+                { name: "s2", keywords: [{ keywordName: "k1" }, { keywordName: "k2" }] }
+              ]
             }
           }
         ]
