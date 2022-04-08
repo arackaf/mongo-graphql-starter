d051d090706195e832ea0e9771a3b8e38d9c3516
Adam Rackis <adamrackis@hotmail.com>
2022-04-07 23:25:34 -0400

    Add fieldOf to test project 1a

--- test/testProject1a/projectSetup.js
+++ test/testProject1a/projectSetup.js
@@ -13,53 +13,54 @@ import {
   objectOf,
   formattedDate,
   JSONType,
-  typeLiteral
+  typeLiteral,
+  fieldOf
 } from "../../src/dataTypes";
 
 export const Author = {
   fields: {
-    name: StringType,
-    birthday: DateType,
-    strings: StringArrayType
+    name: fieldOf(StringType),
+    birthday: fieldOf(DateType),
+    strings: fieldOf(StringArrayType)
   }
 };
 
 export const Book = {
   table: "books",
   fields: {
-    _id: MongoIdType,
-    title: StringType,
-    pages: IntType,
-    weight: FloatType,
-    keywords: StringArrayType,
-    editions: IntArrayType,
-    prices: FloatArrayType,
-    isRead: BoolType,
-    mongoId: MongoIdType,
-    mongoIds: MongoIdArrayType,
-    authors: arrayOf(Author),
-    primaryAuthor: objectOf(Author),
-    strArrs: typeLiteral("[[String]]"),
-    createdOn: DateType,
-    createdOnYearOnly: formattedDate({ format: "%Y" }),
-    jsonContent: JSONType
+    _id: fieldOf(MongoIdType),
+    title: fieldOf(StringType),
+    pages: fieldOf(IntType),
+    weight: fieldOf(FloatType),
+    keywords: fieldOf(StringArrayType),
+    editions: fieldOf(IntArrayType),
+    prices: fieldOf(FloatArrayType),
+    isRead: fieldOf(BoolType),
+    mongoId: fieldOf(MongoIdType),
+    mongoIds: fieldOf(MongoIdArrayType),
+    authors: fieldOf(arrayOf(Author)),
+    primaryAuthor: fieldOf(objectOf(Author)),
+    strArrs: fieldOf(typeLiteral("[[String]]")),
+    createdOn: fieldOf(DateType),
+    createdOnYearOnly: fieldOf(formattedDate({ format: "%Y" })),
+    jsonContent: fieldOf(JSONType)
   }
 };
 
 export const Subject = {
   table: "subjects",
   fields: {
-    _id: MongoIdType,
-    name: StringType
+    _id: fieldOf(MongoIdType),
+    name: fieldOf(StringType)
   }
 };
 
 export const Tag = {
   table: "tags",
   fields: {
-    _id: StringType,
-    name: StringType,
-    count: IntType
+    _id: fieldOf(StringType),
+    name: fieldOf(StringType),
+    count: fieldOf(IntType)
   }
 };
 
@@ -67,7 +68,7 @@ export const ReadonlyTag = {
   table: "tagsReadonly",
   readonly: true,
   fields: {
-    name: StringType,
-    count: IntType
+    name: fieldOf(StringType),
+    count: fieldOf(IntType)
   }
 };
